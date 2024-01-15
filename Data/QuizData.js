51903cdacfff943879b0dda3a067f96b1e049175
Yasir Irshad <117103592+Gulmania@users.noreply.github.com>
2024-01-15 20:55:18 +0500

    Add files via upload

---
+++ Data/QuizData.js
@@ -0,0 +1,27 @@
+export const QuizData = [
+    {
+        question: "Which language runs in a web browser?",
+        options:[ "Java", "C", "Python", "JavaScript"],
+        answer: 4
+    },
+    {
+        question: "What does CSS stand for?",
+        options:["Central Style Sheets", "Cascading Style Sheets", "Cascading Simple Sheets", "Cars SUVs Sailboats"],
+        answer: 2
+    },
+    {
+        question: "What does HTML stand for?",
+        options: ["Hypertext Markup Language", "Hypertext Markdown Language", "Hyperloop Machine Language", "Helicopters Terminals Motorboats Lamborginis"],
+        answer: 1
+    },
+    {
+        question: "What year was JavaScript launched?",
+        options: ["1996", "1995", "1994", "none of the above"],
+        answer:2
+    },
+    {
+        question: " How can you update the state in React?",
+        options: ["setState()", "updateState()", "modifyState()", "changeState()"],
+        answer:1
+    },
+];
